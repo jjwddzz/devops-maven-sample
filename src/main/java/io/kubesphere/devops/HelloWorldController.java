34f6597f8804fae5c195684e7ce8620cde99d97f
jjwddzz <69072410+jjwddzz@users.noreply.github.com>
2024-04-01 08:01:11 +0800

    Update HelloWorldController.java

--- src/main/java/io/kubesphere/devops/HelloWorldController.java
+++ src/main/java/io/kubesphere/devops/HelloWorldController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration
 public class HelloWorldController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String sayHello() {
         return "Really appreciate your star, that's the power of our life.";
     }
